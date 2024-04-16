3c665d71c59fe948ec7cf4cef3398fd39f0d4780
Konstantin Marchenko <asik-net@yandex.ru>
2024-04-16 15:52:26 +0300

    master

--- console/migrations/m240124_165819_boxes_product.php
+++ console/migrations/m240124_165819_boxes_product.php
@@ -15,8 +15,14 @@ class m240124_165819_boxes_product extends Migration
         $this->createTable('{{%boxes_product}}', [
             'id' => $this->primaryKey(),
             'box' => $this->integer()->unsigned()->notNull()->comment('Коробка'),
-            'product' => $this->integer()->unsigned()->notNull()->comment('Продукт')
+            'product' => $this->integer()->comment('Продукт')
         ], $tableOptions);
+
+        $this->createIndex('{{%idx-boxes_product-box}}', '{{%boxes_product}}', 'box');
+        $this->createIndex('{{%idx-boxes_product-product}}', '{{%boxes_product}}', 'product');
+
+        $this->addForeignKey('{{%fk-boxes_product-product}}', '{{%boxes_product}}', 'product', '{{%products}}', 'id', 'CASCADE', 'RESTRICT');
+
     }
 
     public function down() {
